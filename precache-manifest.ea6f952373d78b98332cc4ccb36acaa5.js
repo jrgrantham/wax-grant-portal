20701e216f00d0b06087c7203e4aedf037b91848
James Grantham <jamesgrantham@192.168.1.106>
2021-04-16 11:48:29 +0100

    Updates

---
+++ precache-manifest.ea6f952373d78b98332cc4ccb36acaa5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "38533d8f01eddd4a08e4",
+    "url": "/wax-grant-portal/static/css/main.f6873ef4.chunk.css"
+  },
+  {
+    "revision": "38533d8f01eddd4a08e4",
+    "url": "/wax-grant-portal/static/js/main.9e039666.chunk.js"
+  },
+  {
+    "revision": "ce7ed4f105df540061a3",
+    "url": "/wax-grant-portal/static/js/runtime~main.a0200103.js"
+  },
+  {
+    "revision": "267ccfcca1c46214abb1",
+    "url": "/wax-grant-portal/static/css/2.6074d3f7.chunk.css"
+  },
+  {
+    "revision": "267ccfcca1c46214abb1",
+    "url": "/wax-grant-portal/static/js/2.26d0c3a4.chunk.js"
+  },
+  {
+    "revision": "0c13dd4a6589efc96d96",
+    "url": "/wax-grant-portal/static/js/3.2cbb969b.chunk.js"
+  },
+  {
+    "revision": "f23381715ba315af2502207c10215ff7",
+    "url": "/wax-grant-portal/static/media/background.f2338171.jpg"
+  },
+  {
+    "revision": "86352deb5c10394737edb2c0670b04c0",
+    "url": "/wax-grant-portal/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/wax-grant-portal/precache-manifest.ea6f952373d78b98332cc4ccb36acaa5.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/wax-grant-portal/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
